6e0ea48998e01e4b1a23be092d49d7f5752295af
mdbridge <mdbridge@dc0edc4c-7c48-0410-8122-b089ec71b5fb>
2010-12-30 20:33:45 +0000

    Vocola 2 is now under the MIT license.

--- MacroSystem/core/VocolaUtils.py
+++ MacroSystem/core/VocolaUtils.py
@@ -1,8 +1,28 @@
 ###
 ### VocolaUtils.py - Code used by Vocola's generated Python code
 ###
-### This file is copyright (c) 2002-2010 by Rick Mohr.  It may be redistributed 
-### in any way as long as this copyright notice remains.
+###
+### Copyright (c) 2002-2010 by Rick Mohr.
+### 
+### Permission is hereby granted, free of charge, to any person
+### obtaining a copy of this software and associated documentation
+### files (the "Software"), to deal in the Software without
+### restriction, including without limitation the rights to use, copy,
+### modify, merge, publish, distribute, sublicense, and/or sell copies
+### of the Software, and to permit persons to whom the Software is
+### furnished to do so, subject to the following conditions:
+### 
+### The above copyright notice and this permission notice shall be
+### included in all copies or substantial portions of the Software.
+### 
+### THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+### EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+### MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+### NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+### HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+### WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+### OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+### DEALINGS IN THE SOFTWARE.
 ###
 
 import natlink
@@ -73,7 +93,8 @@ def handle_error(filename, line, command, exception):
     print >> sys.stderr, "    " + command
     print >> sys.stderr, "defined at line " + str(line) + " of " + filename + ","
     print >> sys.stderr, "the following error occurred:"
-    print >> sys.stderr, "    " + type(exception).__name__ + ": " + str(exception)
+    print >> sys.stderr, "    " + exception.__class__.__name__ + ": " \
+        + str(exception)
     #traceback.print_exc()
     #raise exception
 
