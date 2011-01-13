e90a256297424d865ea812a481204b376af6d53a
quintijn <quintijn@dc0edc4c-7c48-0410-8122-b089ec71b5fb>
2011-01-13 16:26:22 +0000

    Setting _DebugLogging to false, produces too much output in dragon logfile (line 1693)

--- natlinkCore/DragCode.cpp
+++ natlinkCore/DragCode.cpp
@@ -1688,11 +1688,13 @@ BOOL CDragonCode::natConnect( IServiceProvider * pIDgnSite, BOOL bUseThreads )
 	}
 	assert( m_pISRCentral == NULL );
 	
-
+//just set _DebugLogging to false (QH, 13/1/2011)(for the time being?)
 #ifdef UseCLR
-	DotNetMisc dnm;
-	int r = dnm.GetDebugLogOptions();
-	_DebugLogging = (BOOL)r;
+	//DotNetMisc dnm;
+	//int r = dnm.GetDebugLogOptions();
+	//_DebugLogging = (BOOL)r;
+	//_DebugLogging = (BOOL)r;
+	_DebugLogging = false;
 #else
 	_DebugLogging = false;
 #endif
