2dce4092004cdc471fc2440702691f5ab96996f5
mdbridge <mdbridge@dc0edc4c-7c48-0410-8122-b089ec71b5fb>
2011-05-25 05:46:35 +0000

    Fixed bug where we misidentified when Vista occurred

--- confignatlinkvocolaunimacro/start_natlinkconfigfunctions.py
+++ confignatlinkvocolaunimacro/start_natlinkconfigfunctions.py
@@ -1,15 +1,13 @@
 import os
 from win32api import ShellExecute, GetVersionEx
 
-path = os.path.join(os.path.dirname(__file__), "start_natlinkconfigfunctions.bat")
-
 # see natlinkstatus.py for windows versions (getWindowsVersion)
 wversion = GetVersionEx()
-if wversion[1] == 2 and wversion[0] >= 6:
+if wversion[3] == 2 and wversion[0] >= 6:
     # Vista and later
     openpar = "runas"
 else:
     openpar = "open"
+path = os.path.join(os.path.dirname(__file__), "start_natlinkconfigfunctions.bat")
 print 'run with "%s": %s'% (openpar, path)
 ShellExecute(0, openpar, path, None, "", 1)
-pass
