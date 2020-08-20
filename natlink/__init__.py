6fb014dbaa8ad429821fb7562e0f8a2a0e38bd69
James Murphy <james@intfxdx.com>
2020-08-20 10:30:36 -0500

    Add pre and post load callbacks in loader.py

--- natlink/__init__.py
+++ natlink/__init__.py
@@ -1,7 +1,9 @@
+from typing import Optional
+
 from ._natlink_core import *
-
 from .config import NoGoodConfigFoundException
-
+from .loader import NatlinkMain
+from .loader import run as run_loader
 from .redirect_output import redirect as redirect_all_output_to_natlink_window
 
-from .loader import run as run_loader
+active_loader: Optional[NatlinkMain] = None
--- natlink/loader.py
+++ natlink/loader.py
@@ -9,7 +9,7 @@ import traceback
 import winreg
 from pathlib import Path
 from types import ModuleType
-from typing import List, Dict, Set, Iterable, Any, Tuple
+from typing import List, Dict, Set, Iterable, Any, Tuple, Callable, Optional
 
 import natlink
 from natlink.config import LogLevel, NatlinkConfig
@@ -23,13 +23,29 @@ class NatlinkMain:
         self.bad_modules: Set[Path] = set()
         self.load_attempt_times: Dict[Path, float] = {}
         self._user: str = ''
+        self._pre_load_callback: Optional[Callable[[], None]] = None
+        self._post_load_callback: Optional[Callable[[], None]] = None
+
+    def set_pre_load_callback(self, pre_load: Optional[Callable[[], None]]) -> None:
+        if pre_load is None:
+            self._pre_load_callback = None
+        elif not callable(pre_load):
+            raise TypeError(f'pre-load callback must be callable, got type: {type(pre_load)}')
+        self._pre_load_callback = pre_load
+
+    def set_post_load_callback(self, post_load: Optional[Callable[[], None]]) -> None:
+        if post_load is None:
+            self._post_load_callback = None
+        elif not callable(post_load):
+            raise TypeError(f'post-load callback must be callable, got type: {type(post_load)}')
+        self._post_load_callback = post_load
 
     @property
     def module_paths_for_user(self) -> List[Path]:
-        return self.module_paths_in_dirs(self.config.directories_for_user(self._user))
+        return self._module_paths_in_dirs(self.config.directories_for_user(self._user))
 
     @staticmethod
-    def module_paths_in_dirs(directories: Iterable[str]) -> List[Path]:
+    def _module_paths_in_dirs(directories: Iterable[str]) -> List[Path]:
 
         def is_script(f: Path) -> bool:
             return f.is_file() and f.name.startswith('_') and f.name.endswith('.py')
@@ -49,22 +65,25 @@ class NatlinkMain:
         return mod_paths
 
     @staticmethod
-    def add_dirs_to_path(directories: Iterable[str]) -> None:
+    def _add_dirs_to_path(directories: Iterable[str]) -> None:
         for d in directories:
             if d not in sys.path:
                 sys.path.insert(0, d)
+
+    def _call_and_catch_all_exceptions(self, fn: Callable[[], None]) -> None:
+        try:
+            fn()
+        except Exception:
+            self.logger.exception(traceback.format_exc())
 
     def unload_module(self, module: ModuleType) -> None:
         unload = getattr(module, 'unload', None)
         if unload is not None:
             self.logger.info(f'unloading module: {module.__name__}')
-            try:
-                unload()
-            except Exception:
-                self.logger.exception(traceback.format_exc())
+            self._call_and_catch_all_exceptions(unload)
 
     @staticmethod
-    def import_module_from_path(mod_path: Path) -> ModuleType:
+    def _import_module_from_path(mod_path: Path) -> ModuleType:
         mod_name = mod_path.stem
         spec = importlib.util.spec_from_file_location(mod_name, mod_path)
         if spec is None:
@@ -87,7 +106,7 @@ class NatlinkMain:
                 last_modified_time = mod_path.stat().st_mtime
                 if force_load or last_attempt_time < last_modified_time:
                     self.logger.info(f'loading previously bad module: {mod_name}')
-                    module = self.import_module_from_path(mod_path)
+                    module = self._import_module_from_path(mod_path)
                     self.bad_modules.remove(mod_path)
                     self.loaded_modules[mod_path] = module
                     return
@@ -98,7 +117,7 @@ class NatlinkMain:
                 maybe_module = self.loaded_modules.get(mod_path)
                 if maybe_module is None:
                     self.logger.info(f'loading module: {mod_name}')
-                    module = self.import_module_from_path(mod_path)
+                    module = self._import_module_from_path(mod_path)
                     self.loaded_modules[mod_path] = module
                     return
                 else:
@@ -108,7 +127,7 @@ class NatlinkMain:
                         self.logger.info(f'reloading module: {mod_name}')
                         self.unload_module(module)
                         del module
-                        module = self.import_module_from_path(mod_path)
+                        module = self._import_module_from_path(mod_path)
                         self.loaded_modules[mod_path] = module
                         return
                     else:
@@ -146,32 +165,41 @@ class NatlinkMain:
 
         importlib.invalidate_caches()
 
+    def trigger_load(self) -> None:
+        self.logger.debug(f'triggering load/reload process')
+        self.remove_modules_that_no_longer_exist()
+        if self._pre_load_callback is not None:
+            self.logger.debug('calling pre-load callback')
+            self._call_and_catch_all_exceptions(self._pre_load_callback)
+        self.load_or_reload_modules(self.module_paths_for_user)
+        if self._post_load_callback is not None:
+            self.logger.debug('calling post-load callback')
+            self._call_and_catch_all_exceptions(self._post_load_callback)
+
     def on_change_callback(self, change_type: str, args: Any) -> None:
-        self.logger.debug(f'on_change_callback called with: change:{change_type}, args:{args}')
+        self.logger.debug(f'on_change_callback called with: change: {change_type}, args: {args}')
         if change_type == 'user':
             user, _profile = args
             if not isinstance(user, str):
-                raise TypeError('unexpected args given to change callback: {args}')
+                raise TypeError(f'unexpected args given to change callback: {args}')
             self._user = user
             if self.config.load_on_user_changed:
-                self.remove_modules_that_no_longer_exist()
-                self.load_or_reload_modules(self.module_paths_for_user)
+                self.trigger_load()
         elif change_type == 'mic' and args == 'on':
             if self.config.load_on_mic_on:
-                self.remove_modules_that_no_longer_exist()
-                self.load_or_reload_modules(self.module_paths_for_user)
+                self.trigger_load()
 
     def on_begin_callback(self, module_info: Tuple[str, str, int]) -> None:
-        self.logger.debug(f'on_begin_callback called with: moduleInfo:{module_info}')
+        self.logger.debug(f'on_begin_callback called with: moduleInfo: {module_info}')
         if self.config.load_on_begin_utterance:
-            self.remove_modules_that_no_longer_exist()
-            self.load_or_reload_modules(self.module_paths_for_user)
+            self.trigger_load()
 
     def start(self) -> None:
         self.logger.info('starting natlink loader')
-        self.add_dirs_to_path(self.config.directories)
+        natlink.active_loader = self
+        self._add_dirs_to_path(self.config.directories)
         if self.config.load_on_startup:
-            self.load_or_reload_modules(self.module_paths_for_user)
+            self.trigger_load()
         natlink.setBeginCallback(self.on_begin_callback)
         natlink.setChangeCallback(self.on_change_callback)
 
