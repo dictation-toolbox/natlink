8ca8f51c46eb66e7d5ffed509f245f6ba89e1219
Quintijn <q.hoogenboom@antenna.nl>
2021-07-15 13:17:22 +0200

    working on natlinkstatus in natlinkstartup.py

--- src/natlinkcore/natlinkstartup.py
+++ src/natlinkcore/natlinkstartup.py
@@ -1,31 +1,35 @@
-from   stat import *    # file statistics
+"""this module performs several (unimacro) actions at Natlink startup time
+"""
 import os               # access to file information
 import os.path          # to parse filenames
+import stat
 import re
 import shutil
 import sys
+import natlinkstatus
+status = natlinkstatus.NatlinkStatus()
 
 print(f'natlinkstartup: {__file__}')
 
-from natlinkcore import natlinkstatus
 #
 # This function is called by natlinkmain when starting up just before
 # loading grammars for the first time:
 #
 def start():
+    """starting two features of Vocola
+    """
     print('--- natlinkstartup starting...')
+    print(status.getNatlinkStatusString())
     updateUnimacroHeaderIfNeeded()
     create_new_language_subdirectory_if_needed()
-
-
-
 ##
 ## Update user's copy of Unimacro.vch if a more recent version is
 ## available
 ##
 
 def updateUnimacroHeaderIfNeeded():
-    status = natlinkstatus.NatlinkStatus()
+    """check the Unimacro header for include Unimacro.vch
+    """
     if not status.getVocolaTakesUnimacroActions(): 
         return
         
@@ -71,7 +75,10 @@ Dragon, if you want to use the updated version of this file."""% (destDir, sourc
 
 # Returns the modification time of a file or 0 if the file does not exist:
 def vocolaGetModTime(file):
-    try: return os.stat(file)[ST_MTIME]
+    """get date/time of file, 0 if not exists
+    """
+    #pylint:disable=C0321
+    try: return os.stat(file)[stat.ST_MTIME]
     except OSError: return 0        # file not found
 
 ## 
@@ -79,8 +86,12 @@ def vocolaGetModTime(file):
 ## 
 
 def create_new_language_subdirectory_if_needed():
-    status        = natlinkstatus.NatlinkStatus()
-    VocolaEnabled = not not status.getVocolaUserDirectory()
+    """create a language subdirectory for Vocola if needed
+    
+    This is the case when the user language is non-English (not "enx"), and
+    the option VocolaTakesLanguages is set.
+    """
+    VocolaEnabled = bool(status.getVocolaUserDirectory())
     language      = status.getLanguage()
     commandFolder = status.getVocolaUserDirectory()
     if not os.path.isdir(commandFolder):                      
@@ -96,6 +107,8 @@ def create_new_language_subdirectory_if_needed():
                 copyToNewSubDirectory(uDir, uDir2)
 
 def copyToNewSubDirectory(trunk, subdirectory):
+    """utility function
+    """
     for f in os.listdir(trunk):
         if f.endswith('.vcl'):
             copyVclFileLanguageVersion(os.path.join(trunk, f),
@@ -106,15 +119,14 @@ def copyVclFileLanguageVersion(Input, Output):
     """
     # let include lines to relative paths point to the folder above ..\
     # so you can take the same include file for the alternate language.
-    status = NatlinkStatus()
     reInclude = re.compile(r'^(\s*include\b\s*[\'\"]?)([^\s;=][^;=\n]*\s*;.*)$')
     Input     = os.path.normpath(Input)
     Output    = os.path.normpath(Output)
-    input     = open(Input, 'r').read()
+    inputString = open(Input, 'r').read()
     output    = open(Output, 'w')
     language      = status.getLanguage()
     output.write("# vocola file for alternate language: %s\n"% language)
-    lines = list(map(lambda s: s.strip(), str(input).split('\n')))
+    lines = list(map(lambda s: s.strip(), inputString.split('\n')))
     for line in lines:
         m = reInclude.match(line)
         if m:
