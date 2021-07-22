0dcbef49bc67c8cb7c0703e9d66e773e17927701
Doug Ransom <doug.ransom@gmail.com>
2021-07-22 13:18:18 -0700

    removed debugging,updated build date, add some scripts to help with publishing and registering, deleted some cruft binaries, removed dependency on a microsoft dll

--- reg_dev_natlink.ps1
+++
@@ -1,7 +0,0 @@
-#register the natlink.pyd build by visual studio locally
-
-$natlink_binary=Get-ChildItem NatlinkSource/Debug/*.pyd
-echo "unregistring natlink"
-natlinkconfig_cli -R
-echo registering natlink binary "$natlink_binary"
-natlinkconfig_cli --dev_natlink=$natlnk_binary -r
