dbc9f21493673214cebfb0557e1eaa42761c5bef
Quintijn <q.hoogenboom@antenna.nl>
2021-08-11 16:01:38 +0200

    plus update readme.md (Doug), release 5.1.5

--- src/natlinkcore/__init__.py
+++ src/natlinkcore/__init__.py
@@ -52,7 +52,7 @@ import sys
 import pathlib
 
 ## change this before a new release on pypi is made:
-__version__ = '5.1.4'    # hope to have solved DICTATIONTOOLBOXUSER trick again
+__version__ = '5.1.5'    # hope to have solved DICTATIONTOOLBOXUSER trick again + update readme.md
 # __version__ = '5.1.3'    # try DICTATIONTOOLBOXUSER trick again and getDtactionsDirectory
 # __version__ = '5.1.2'    # introduce DICTATIONTOOLBOXUSER for alternative user directory
 # __version__ = '5.1.1'    # no registry depencde any more
