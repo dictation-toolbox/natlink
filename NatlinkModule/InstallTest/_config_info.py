54a2cfd76288ee43f796ed80074ea41143aa7bac
Quintijn Hoogenboom <q.hoogenboom@antenna.nl>
2022-02-13 16:30:27 +0100

    a few typos

--- NatlinkModule/InstallTest/_config_info.py
+++ NatlinkModule/InstallTest/_config_info.py
@@ -2,13 +2,14 @@
 
 It gives config information
 """
-
+#pylint:disable=W0212
 import sys
 import sysconfig
 from pprint import pprint
+
 print()
 print()
-print("I'm _meet_natlink.py and I'm being loaded, too, on my own.")
+print("I'm _config_info.py and I'm being loaded, too, on my own.")
 print()
 print("This is your Python configuration as per sysconfig:")
 print("---------------------------------------------------")
@@ -16,5 +17,5 @@ sysconfig._main()
 print("This is your Python system path sys.path:")
 print("-----------------------------------------")
 pprint(sys.path)
-print("End of _meet_natlink.py info.")
+print("End of _config_info.py info.")
 print("-----------------------------")
--- NatlinkModule/InstallTest/config_instructions.py
+++ NatlinkModule/InstallTest/config_instructions.py
@@ -12,36 +12,40 @@ this_dir, this_filename = __file__.rsplit('\\', 1)
 
 print()
 print()
-print(f' This is the __init__.py from directory {this_dir}')
+print(f'This is the file "__init__.py" from directory "{this_dir}"')
 print('This directory holds the default "natlink.ini" file, when it is not')
 print('found in any of the other possible directories.')
 print()
-
-print(r'By default this config file is stored in ~\.natlink\natlink.ini')
-print(f'with "~" your HOME directory: {home}')
-
+print(r'By default this config file should be stored in "~\.natlink\natlink.ini"')
+print(f'with "~" being your HOME directory: "{home}"')
+print()
 print(r'Alternatively you can store your config file in  "~\Documents\.natlink\natlink.ini"')
-
+print()
 print('You can also choose to set the environment variable "DICTATIONTOOLBOXHOME" to ')
 print('a self chosen directory. Environment variables can be used in the ')
 print('definition of "DICTATIONTOOLBOXHOME".')
+print()
 
 dictationtoolboxhome = getenv("DICTATIONTOOLBOXHOME")
 if dictationtoolboxhome:
-    print()
-    print(f'You have set your "DICTATIONTOOLBOXHOME" to:')
+
+    print('You have set your "DICTATIONTOOLBOXHOME" to:')
     print(f'\t{dictationtoolboxhome}, so please ensure you ')
-    print(f'\tcreate a subdirectory ".natlink",')
+    print('\tcreate a subdirectory ".natlink",')
     print(f'\tand copy "{this_dir}\\natlink.ini" into {dictationtoolboxhome}\\.natlink\\natlink.ini')
 else:
     print('You did not set "DICTATIONTOOLBOXHOME"')
-    
+
+print()  
 print('When you have chosen your location for your "natlink.ini" file, ')
-print('please copy the file "{this_dir}\\natlink.ini" into this location')
-print('and proceed with manual configuration or via natlinkconfig program (to be fixed yet)')
+print(f'please copy the file "{this_dir}\\natlink.ini" into this location.')
+print('====DO NOT FORGET the ".natlink" subdirectory!====')
+print()
+print('Then proceed with manual configuration or with')
+print('configuration or with via natlinkconfig program (to be fixed yet)')
  
 print()
 print()
 
-print('After all these steps, restart Dragon...')
+print('After all these steps, restart Dragon...\n\n\n')
 
