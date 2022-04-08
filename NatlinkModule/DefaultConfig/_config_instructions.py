618c185dc2f4e1bbaaa52d5e65b91ce1a8c8c8e5
Quintijn Hoogenboom <q.hoogenboom@antenna.nl>
2022-04-08 16:01:08 +0200

    improving config instructions at the fallback locations of the natlink.ini file

--- NatlinkModule/DefaultConfig/_config_instructions.py
+++ NatlinkModule/DefaultConfig/_config_instructions.py
@@ -16,58 +16,36 @@ print()
 print(f'This is the file "{this_filename}" from directory "{this_dir}"')
 print('This directory holds the default "natlink.ini" file, when it is not found in any of the other possible directories.')
 print()
-print(r'By default this config file should be stored in "~\.natlink\natlink.ini",')
-print(f'\twith "~" being your HOME directory: "{home}"')
+print(r'By default this config directory should be: "~\.natlink", with "~" being your HOME directory: "{home}"')
 print()
-print('There are two other ways to configure name and/or location of your "natlink.ini" file:')
+print('There is also a custom way to configure the directory your "natlink.ini" file then should be:')
 print()
-print('1. specify the environment variable "NATLINK_INI", pointing to an existing file,')
+print('Specify the environment variable "NATLINK_USERDIR", which should point to an existing directory.')
 print()
-print('2. define the environment variable "DICTATIONTOOLBOXHOME" to a valid directory.')
-print('   This directory should hold a subdirectory ".natlink" and a config file "natlink.ini".')
+print('When this directory does not hold the file "natlink.ini", you can copy it from "{this_dir}",')
+print('or run the configure program of Natlink ("configurenatlink.pyw" or "natlinkconfigfunctions.py").')
+print('The default version of "natlink.ini" will be copied into the correct place, and you can proceed with ')
+print('configuring the additional options in order to get started with using Natlink.')
 
-natlink_ini = getenv("NATLINK_INI")
-dictationtoolboxhome = getenv("DICTATIONTOOLBOXHOME")
+natlink_userdir = getenv("NATLINK_USERDIR")
 
-if natlink_ini:
-    if isfile(natlink_ini):
-        print(f'You specified "NATLINK_INI" to "{natlink_ini}", so the Natlink should not appear here.')
-        print('Please try/retry to copy the sample "natlink.ini" file into this path.')
+if natlink_userdir:
+    if isdir(natlink_userdir):
+        print(f'You specified "NATLINK_USERDIR" to "{natlink_userdir}".')
+        print('So the Natlink should startup with these messages. ')
+        print('Probably you do not have a proper "natlink.ini" file into this path.')
+        print('Please run the configure program of Natlink ("configurenatlink.pyw" or "natlinkconfigfunctions.py")')
     else:
-        print(f'You specified "NATLINK_INI" to "{natlink_ini}", but this is not a valid path')
-        print('Please copy the sample "natlink.ini" file into this path,\n\tor change/remove the environment variable "NATLINK_INI".')
-        
-elif dictationtoolboxhome:
-    print('You have set your "DICTATIONTOOLBOXHOME" to:')
-    if not os.path.isdir(dictationtoolboxhome):
-        print(f'Please create this directory "{dictationtoolboxhome}"\n\tand a subdirectory ".natlink"\n\tand then copy your sample "natlink.ini" file into this subdirectory')
-    else:
-        subdirectory = join(dictationtoolboxhome, '.natlink')
-        if not os.path.isdir(subdirectory):
-            print(f'Please create a subdirectory ".natlink" in {dictationtoolboxhome}\n\tand then copy your sample "natlink.ini" file into this subdirectory')
-        else:
-            inifile = join(subdirectory, 'natlink.ini')
-            if not os.path.isfile(inifile):
-                print(f'Please copy your sample "natlink.ini" file into "{subdirectory}"')
-    print()
-    print('When you do not want to user environment variable "DICTATIONTOOLBOXHOME", remove it from your system')
-    print()
+        print(f'You specified "NATLINK_USERDIR" to "{natlink_userdir}", but this is not a valid directory path')
 else:
     print()
-    print('You did not set "NATLINK_INI" or "DICTATIONTOOLBOXHOME" on your windows system')
+    print('You did not set "NATLINK_USERDIR" on your windows system')
     print(f'The config file "natlink.ini" should now be in "{home}\\.natlink\\natlink.ini"')
     print()
-    print(f'So... please copy the file "{this_dir}\\natlink.ini" into this location.')
-    print('====DO NOT FORGET the ".natlink" subdirectory!')
+    print(f'So... please copy the file "{this_dir}\\natlink.ini" into this location, or (better)')
+    print('Please run the configure program of Natlink ("configurenatlink.pyw" or "natlinkconfigfunctions.py")')
 
 print()
-print('Then proceed with manual configuration or with')
-print('configuration or with via natlinkconfig program (to be fixed yet).')
-print('When manually configuring "natlink.ini", comment out the line starting with "default_config"')
 
- 
-print()
-print()
-
-print('After all these steps, restart Dragon and good luck...\n\n\n')
+print('After all these steps, (re)start Dragon and good luck...\n\n\n')
 
