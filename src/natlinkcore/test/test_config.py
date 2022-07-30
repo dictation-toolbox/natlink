b54500ece1822f5ffb2010fde78093bc4591cee9
Doug Ransom <doug.ransom@gmail.com>
2022-07-30 10:44:23 -0700

    loading a NatlnkConfig from a sample, test passed

--- src/natlinkcore/test/test_config.py
+++ src/natlinkcore/test/test_config.py
@@ -29,9 +29,11 @@ def sample_config(sample_name) -> 'NatlinkConfig':
     config = NatlinkConfig.from_file(sample_ini)
     return config
 
-@pytest.fixture()
-def few_settings():
-    return sample_config("few_settings.ini")
+#@pytest.fixture()
+#def few_settings():
+#    return sample_config("few_settings.ini")
+
+few_settings = pytest.fixture( lambda : sample_config("few_settings.ini"))
 
 def test_load_an_ini_file(few_settings):
         test_ini = few_settings 
