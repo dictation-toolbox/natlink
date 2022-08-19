b72889df07d4ca3118ff118d60f940d98655b60d
Doug Ransom <doug.ransom@gmail.com>
2022-08-19 11:35:18 -0700

    Revert changes, try a differnet way for playstring, though it works at this point.

--- NatlinkSource/DragonCode.cpp
+++ NatlinkSource/DragonCode.cpp
@@ -126,8 +126,7 @@
 #include "COM/appsupp.h"
 #include "MessageWindow.h"
 #include "Exceptions.h"
-#include <string>
-#include <memory>
+#include <cstring>
 
 // defined in PythWrap.cpp
 CResultObject * resobj_new();
@@ -1720,20 +1719,9 @@ BOOL CDragonCode::natConnect( IServiceProvider * pIDgnSite, BOOL bUseThreads )
 		__uuidof(IDgnSSvcOutputEvent), (void**)&m_pIDgnSSvcOutputEvent);
 	RETURNIFERROR( rc, "IDgnSSvcOutputEvent::QueryInterface(DgnSSvcOutputEvent)" );
 
-	rc = pSpchSvc->QueryInterface(
-		__uuidof(IDgnSSvcOutputEventA), (void**)&m_pIDgnSSvcOutputEventA);
-	RETURNIFERROR( rc, "IDgnSSvcOutputEventA::QueryInterface(DgnSSvcOutputEventA)" );
-
-
 	rc = m_pIDgnSSvcOutputEvent->QueryInterface(
 		__uuidof(IDgnSSvcInterpreter), (void**)&m_pIDgnSSvcInterpreter );
 	RETURNIFERROR( rc, "IDgnSSvcOutputEvent::QueryInterface(IDgnSSvcInterpreter)" );
-
-
-	rc = m_pIDgnSSvcOutputEvent->QueryInterface(
-		__uuidof(IDgnSSvcInterpreterA), (void**)&m_pIDgnSSvcInterpreterA );
-	RETURNIFERROR( rc, "IDgnSSvcOutputEvent::QueryInterface(IDgnSSvcInterpreterA)" );
-
 
 	// we use this interface in getCurrentModule
 
@@ -1865,11 +1853,9 @@ BOOL CDragonCode::natDisconnect()
 	// release all our intefaces
 	m_pIDgnSREngineControl = NULL;
 	m_pIDgnSSvcOutputEvent = NULL;
-	m_pIDgnSSvcOutputEventA = NULL;
 	m_pIDgnExtModSupStrings = NULL;
 	m_pIServiceProvider = NULL;
 	m_pIDgnSSvcInterpreter = NULL;
-	m_pIDgnSSvcInterpreterA = NULL;
 	m_pIDgnSRTraining = NULL;
 	m_pISRCentral = NULL;
 
@@ -1947,45 +1933,11 @@ PyObject * CDragonCode::getCallbackDepth()
 	return Py_BuildValue( "i", m_nCallbackDepth );
 }
 
-
-static std::string stringinfo(char const word[])
-{
-	std::string by;
-	for(int i=0; i<strlen(word); i++)
-	{			
-		char buf[100];
-		unsigned char c = word[i];
-		unsigned int d=c;
-
-		sprintf(buf,"%i:%u:%c ",i,d,c );
- 
-		by.append(buf);
-	}
-	return by;
-}
-
-static std::string w_stringinfo(wchar_t const word[])
-{
-	std::string by;
-	for(int i=0; i<wcslen(word); i++)
-	{			
-		char buf[100];
-		unsigned char c = word[i];
-		unsigned int d=c;
-
-		snprintf(buf,sizeof buf,"%i:%u:%c ",i,d,c );
- 
-		by.append(buf);
-	}
-	return by;
-}
-
- 
 //---------------------------------------------------------------------------
 
 BOOL CDragonCode::playString( const char * pszKeys, DWORD dwFlags )
 {
-	HRESULT rc=0;
+	HRESULT rc;
 
 	NOTBEFORE_INIT( "playString" );
 	NOTDURING_INIT( "playString" );
@@ -1996,47 +1948,18 @@ BOOL CDragonCode::playString( const char * pszKeys, DWORD dwFlags )
 	static DWORD dwUnique = 1;
 	DWORD dwClientCode = ++dwUnique;
 
-	DWORD dwNumUndo=0;
-		
-
+	DWORD dwNumUndo;
 	#ifdef UNICODE
-
-//	OutputDebugStringA("ignoring input, just sending out `naïve'");
-
-	int const inputCodePage = CP_UTF8;
-	int const outputCodePage = 1252;
-
-
-	//get a wide unicode string from the unicode string.  Be better to change pytwrap to just give us
-	//unicode strings.
-	int size_needed = 10+ ::MultiByteToWideChar( inputCodePage, 0, pszKeys, -1, NULL, 0 );
-	BSTR pszKeysW = new TCHAR[ size_needed ];
-	memset(pszKeysW,0,(sizeof pszKeysW[0]*size_needed));
-
-	std::unique_ptr<TCHAR> pdzKesWMem(pszKeysW);   
-
-	::MultiByteToWideChar( inputCodePage, MB_PRECOMPOSED, pszKeys, -1, pszKeysW, size_needed );
-
-	//now convert to to single byte code page for windows
-	int mbSizeNeeded = 10+WideCharToMultiByte(outputCodePage,0,pszKeysW,-1,0,0,0,0);
-	char * mbString = new char[mbSizeNeeded];
-	memset(mbString,0,sizeof mbString[0]*mbSizeNeeded);
-	std::unique_ptr<char> mbPtr(mbString);  
-	WideCharToMultiByte(outputCodePage,0,pszKeysW,-1,mbString,mbSizeNeeded,0,0);
-
-	std::string msg1("Windows String: ");
-	msg1.append(mbString);
-	OutputDebugStringA(msg1.c_str());
-
-	// use the interface that takes char strings, not wide strings.
-
-	rc = m_pIDgnSSvcOutputEventA->PlayString(
-		mbString,	// string to send
-		dwFlags,		// flags
-		0xFFFFFFFF,		// delay (-1 for app specific delay)
-		dwClientCode,	// to identify which WM_PLAYBACK is ours
-		&dwNumUndo );
-
+		/*int size_needed = ::MultiByteToWideChar( CP_UTF8, 0, pszKeys, -1, NULL, 0 );
+		CPointerChar pszKeysW = new TCHAR[ size_needed ];
+		::MultiByteToWideChar( CP_UTF8, 0, pszKeys, -1, pszKeysW, size_needed );*/
+		CComBSTR bstrKeys( pszKeys );
+		rc = m_pIDgnSSvcOutputEvent->PlayString(
+			bstrKeys,	// string to send
+			dwFlags,		// flags
+			0xFFFFFFFF,		// delay (-1 for app specific delay)
+			dwClientCode,	// to identify which WM_PLAYBACK is ours
+			&dwNumUndo );	// not used (number of backspaces needed to undo)
 	#else
 		rc = m_pIDgnSSvcOutputEvent->PlayString(
 			pszKeys, 	  // string to send
@@ -2045,7 +1968,6 @@ BOOL CDragonCode::playString( const char * pszKeys, DWORD dwFlags )
 			dwClientCode, // to identify which WM_PLAYBACK is ours
 			&dwNumUndo ); // not used (number of backspaces needed to undo)
 	#endif
-
 
 	RETURNIFERROR( rc, "IDgnSSvcOutputEvent::PlayString" );
 
@@ -2399,9 +2321,6 @@ BOOL CDragonCode::execScript(
 	{
 		pszComment = "execScript";
 	}
-	std::string msg("execScript: ");
-	msg.append(pszScript);
-	OutputDebugStringA(msg.c_str());
 
 	// Although this is optional, if we check the script syntax first, we
 	// can report errors in a cleaner way.
