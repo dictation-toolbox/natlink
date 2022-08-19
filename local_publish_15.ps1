8ec2ac65d8181810d19a1803fc09e859d044d964
Doug Ransom <doug.ransom@gmail.com>
2022-08-19 08:01:18 -0700

    updated local publish script

--- local_publish_15.ps1
+++ local_publish_15.ps1
@@ -1,6 +1,7 @@
 
 $dest_folder = "C:\Program Files (x86)\Natlink\site-packages\natlink\"
 $dest_pyd = "_natlink_core.pyd"
+$test_pdb = "_natlink_core.pdb"
 $dest_pyd_file = $dest_folder+$dest_pyd
 echo "hi"
 echo $dest_pyd_file
@@ -12,4 +13,5 @@ $msg = "updates __init__.py and _natlinkcore.pyd  from _natlinkcore15.pyd to " +
 
 echo $msg
 Copy-Item  -Destination $dest_pyd_file build\NatlinkSource\Debug\_natlink_core15.pyd
+Copy-Item  -Destination $dest_pdb_file build\NatlinkSource\Debug\_natlink_core15.pdb
 Copy-Item  -Destination $dest_folder natlinksource\__init__.py
