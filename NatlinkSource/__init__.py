401e88a64f443ce5f3c7053aac9a1373549befc8
Quintijn Hoogenboom <q.hoogenboom@antenna.nl>
2022-08-22 17:08:33 +0200

    try to enhance recognitionMimic with wide string trick in __init__.py

--- NatlinkSource/__init__.py
+++ NatlinkSource/__init__.py
@@ -5,6 +5,7 @@ print("Import natlink")
 import locale
 from ._natlink_core import execScript as _execScript
 from ._natlink_core import playString as _playString
+from ._natlink_core import recognitionMimic as _recognitionMimic
 
 def lmap(fn,iter):
     return list(map(fn,iter))
@@ -18,6 +19,11 @@ def execScript(script,args=None):
         args = []
     script_w=toWindowsEncoding(script)
     return _execScript(script_w,args)    
+
+def recognitionMimic(words):
+    words_w = lmap(toWindowsEncoding, words)
+    print(f'recognitionMimic, words: {words}, words_w: {words_w}')
+    return _recognitionMimic(words_w)
 
 def toWindowsEncoding(str_to_encode):
     return str_to_encode.encode('Windows-1252')
