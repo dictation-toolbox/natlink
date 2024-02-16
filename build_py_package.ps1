d5f9bb9ef06f24427b6aabd9f6cedfd3e37df2b0
Doug Ransom <doug.ransom@gmail.com>
2024-02-16 14:58:42 -0800

    removing obsolete files.

--- build_py_package.ps1
+++
@@ -1,4 +0,0 @@
-cp .\build\NatlinkSource\Debug\_natlink_core_legacy.pdb src/natlink
-cp .\build\NatlinkSource\Debug\_natlink_core.pdb src/natlink
-cp .\build\NatlinkSource\Debug\_natlink_core_legacy.pyd src/natlink
-cp .\build\NatlinkSource\Debug\_natlink_core.pyd src/Natlink
--- pip_install.ps1
+++
@@ -1,4 +0,0 @@
-#not the forward slashes for pip
-$dest_folder = "C:/Program Files (x86)/Natlink/site-packages/natlink/"
- 
-py -m pip install -t "$dest_folder" .\dist\natlink-1.0-py3-none-any.whl
